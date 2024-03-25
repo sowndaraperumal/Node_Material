31081bdc51b58b5ccaaeb7abee24d2e7981471d0
sowndaraperumal <sowndaraperumal18@gmail.com>
2024-03-25 18:31:53 +0530

    node_study file

---
+++ Study/sample.js
@@ -0,0 +1,15 @@
+//console.log("hello")
+//console.log(global)
+
+const os= require('os');
+const path = require('path');
+
+//console.log(os.type())
+//console.log(os.version())
+//console.log(os.homedir())
+console.log(__dirname,__filename);
+console.log("---------------------")
+console.log(path.dirname(__filename))
+console.log(path.basename(__filename))
+console.log(path.extname(__filename))
+console.log(path.parse(__filename))
---
+++ Study/server.js
@@ -0,0 +1,5 @@
+const math=require("./math");
+console.log(math.add(1,2))
+console.log(math.sub(342,34))
+console.log(math.mul(3,5))
+console.log(math.div(4,3))
